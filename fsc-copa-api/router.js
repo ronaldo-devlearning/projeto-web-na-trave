82bb8bad5a953bd702d6c73c790979a2c7c66f3a
Ronaldo Rocha <ronaldo.devlearning@gmail.com>
2022-12-21 10:12:57 -0300

    Backend configurado, adicionadas as dependências koa, koa-bodyparser, @koa/router e @koa/cors

---
+++ fsc-copa-api/router.js
@@ -0,0 +1,11 @@
+import Router from '@koa/router'
+
+export const router = new Router()
+
+router.get('/', async ctx => {
+    ctx.body = { ola: "Mundo" }
+})
+
+router.get('/users', async ctx => {
+    ctx.body = { ola: "Users" }
+})
